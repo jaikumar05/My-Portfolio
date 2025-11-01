0b8a02213f7d6775b27b1f4d3fae5c4e37f5ccb1
Jai Kumar <jaivb108@gmail.com>
2025-11-01 16:33:28 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var today = new Date();
+    var day = today.getDate();
+    var year = today.getFullYear();
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var month = months[today.getMonth()];
+    
+    var locationElement = document.getElementById('location-date');
+    if (locationElement) {
+        locationElement.innerHTML = 'Jaipur, ' + day + ' ' + month + ' ' + year;
+    }
+};
+window.addEventListener("load", (event) => {
+  new cursoreffects.ghostCursor();
+});
